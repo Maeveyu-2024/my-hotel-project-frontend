80f2a22fc2d9d7c867f7730eb4fac3b6d6f26c75
meave_yu <1138142726@qq.com>
2024-09-19 18:38:52 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';  // 或者 'bootstrap.min.js'
+import AddRoom from './components/room/AddRoom'
+
+function App() {
+  return <>
+  <AddRoom/>
+  </>
+}
+
+export default App
